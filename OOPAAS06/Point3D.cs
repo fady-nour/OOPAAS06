522ec763c9e23e0873582fdcf8eb1bd9e80957e5
Fadynour <fadynour194@gmail.com>
2025-08-10 20:14:23 +0300

    Add project files.

---
+++ OOPAAS06/Point3D.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPAAS06
+{
+    #region Q1
+
+    public class Point3D : IComparable<Point3D>, ICloneable
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+            public double Z { get; set; }
+
+            public Point3D() : this(0, 0, 0) { }
+            public Point3D(double x) : this(x, 0, 0) { }
+            public Point3D(double x, double y) : this(x, y, 0) { }
+            public Point3D(double x, double y, double z)
+            {
+                X = x; Y = y; Z = z;
+            }
+
+            public override string ToString()
+            {
+                return $"Point Coordinates: ({X}, {Y}, {Z})";
+            }
+
+            public int CompareTo(Point3D other)
+            {
+                if (this.X != other.X)
+                    return this.X.CompareTo(other.X);
+                return this.Y.CompareTo(other.Y);
+            }
+
+            public object Clone()
+            {
+                return new Point3D(this.X, this.Y, this.Z);
+            }
+        #endregion
+    }
+}
---
+++ OOPAAS06/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace OOPAAS06
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            #region q1
+            Point3D P1 = ReadPointFromUser("P1");
+          Point3D P2 = ReadPointFromUser("P2");
+
+        Console.WriteLine(P1);
+            Console.WriteLine(P2);
+
+          
+            Console.WriteLine(P1 == P2
+                ? "P1 and P2 reference are the same"
+                : "P1 and P2 reference are different");
+
+ 
+            Point3D[] points = {
+                new Point3D(5, 2, 1),
+                new Point3D(3, 4, 7),
+                new Point3D(3, 1, 0),
+                new Point3D(7, 8, 9)
+            };
+
+        Array.Sort(points);
+            Console.WriteLine("\nSorted Points:");
+            foreach (var p in points)
+                Console.WriteLine(p);
+
+            Point3D cloned = (Point3D)P1.Clone();
+        Console.WriteLine($"\nCloned Point: {cloned}");
+            #endregion
+        }
+
+        #region q1
+
+
+        static Point3D ReadPointFromUser(string pointName)
+        {
+            double x = ReadDouble($"{pointName} - Enter X: ");
+            double y = ReadDouble($"{pointName} - Enter Y: ");
+            double z = ReadDouble($"{pointName} - Enter Z: ");
+            return new Point3D(x, y, z);
+         
+        }   static double ReadDouble(string message)
+        {
+            double value;
+            Console.Write(message);
+            while (!double.TryParse(Console.ReadLine(), out value))
+            {
+                Console.Write("Invalid input, try again: ");
+            }
+            return value;
+        }
+#endregion
+    }
+}
